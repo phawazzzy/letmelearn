45f33a7d757d3705e69ce92dc4656633823eaa1e
kareem fawas <phawazzzy@gmail.com>
2020-04-21 13:49:22 +0100

    challenge done

--- lib/main.dart
+++ lib/main.dart
@@ -5,22 +5,23 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Sample App',
+      title: 'I am Poor',
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
         appBar: AppBar(
+          elevation: 4.0,
+          backgroundColor: Colors.blueGrey[700],
           centerTitle: true,
-          title: Text('I Am Rich'),
-          backgroundColor: Colors.blueGrey[900],
+          title: Text('I am Poor app'),
         ),
         body: Center(
           child: Image(
             image: AssetImage('images/diamond.png'),
           ),
         ),
+
       ),
     );
   }
